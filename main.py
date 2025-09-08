b0c407ae7d422dad5fe350379df3fe05c42e9e4c
Moffee <55623228+ZachMoffee@users.noreply.github.com>
2025-09-08 20:44:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.models import Sequential # type: ignore
 from tensorflow.keras.layers import TextVectorization, Dense, Embedding, GlobalAveragePooling1D # type: ignore
 from tensorflow.keras.optimizers import Adam # type: ignore
-
 # Step 1: Load the CSV file with statements and associated illnesses
 def load_csv(file_path="new.csv"):
     try:
